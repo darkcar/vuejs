f8732427b2f50cde69896b645eb0ecc91ff7d565
Xuegang Wang <darkcar@gmail.com>
2019-04-30 22:59:11 -0600

    Add some features
    #1. Add custom filter to date.
    #2. Add page loading focus, aka customized directives.
    #3. Add custom keyboard identifier, through querying keycode and update the reference.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 The vue project with static content. 
 
 Step by step learning procedure. 
+
+Video - day 02 - 13
--- script/myScript.js
+++ script/myScript.js
@@ -1,3 +1,55 @@
+// Global filter, format date
+// Vue.filter('dateFormat', function(dateStr) {
+//     // Get date object 
+//     var dt = new Date(dateStr);
+//     // Date format: yyyy-MM-dd
+//     var y = dt.getFullYear();
+//     var m = dt.getMonth() + 1;
+//     var d = dt.getDate();
+//     // return y + "-" + m + "-" + d;
+//     return `${y}-${m}-${d}`;
+// });
+// All the vm instance can share this filter. 
+Vue.filter('dateFormat', function(dateStr, pattern="") {
+    var dt = new Date(dateStr);
+    var y = dt.getFullYear();
+    var m = (dt.getMonth() + 1).toString().padStart(2, '0');
+    var d = dt.getDate().toString().padStart(2, '0');
+
+    if(pattern && pattern.toLowerCase() === 'yyyy-mm-dd') {
+        return `${y}-${m}-${d}`;
+    } else {
+        // Fill extra 0s for the string.
+        var hh = dt.getHours().toString().padStart(2, '0');;
+        var mm = dt.getMinutes().toString().padStart(2, '0');;
+        var ss = dt.getSeconds().toString().padStart(2, '0');;
+        return `${y}-${m}-${d} ${hh}:${mm}:${ss}`;
+    }
+});
+
+// Custom Keyboard identifier - query the keycode of keyboard element. 
+Vue.config.keyCodes.f2=113;
+
+// Custom Directives - Global
+/**
+ * @arg1: Directive name, no starting with v-
+ * @arg2: 
+ */
+Vue.directive(
+    'focus', {
+        // @param: el: dom element
+        bind: function(el) { // bind element, not in dom yet. 
+            el.focus(); // not working.
+        },
+        inserted: function(el) { // inserted element to dom, call the function
+            el.focus();
+        }, 
+        updated: function() { // VNode updates, execute update one or more times.
+
+        }
+    }
+);
+
 var vm = new Vue({
     el: '#app',
     data: {
@@ -40,3 +92,4 @@ var vm = new Vue({
         }
     }
 });
+
